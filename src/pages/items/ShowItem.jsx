df2e8669dd50c7852ee9f377d759d0512f52afc9
João Vitor Neves <jvneves_marq@hotmail.com>
2023-11-08 18:03:14 -0300

    first commit

---
+++ src/pages/items/ShowItem.jsx
@@ -0,0 +1,5 @@
+function ShowItem() {
+  return <h2>Exibir Item</h2>;
+}
+
+export default ShowItem;
---
+++ src/pages/items/UpdateItem.jsx
@@ -0,0 +1,5 @@
+function UpdateItem() {
+  return <h2>Atualizar Item</h2>;
+}
+
+export default UpdateItem;
